8d18a9e1d1d5dcd290bb19d59092b9291057aac0
max-miller <max.samuel.miller@gmail.com>
2025-12-14 13:51:44 -0500

    swapping back database

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,20 +80,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ["PGDATABASE"],
-#         'USER': os.environ["PGUSER"],
-#         'PASSWORD': os.environ["PGPASSWORD"],
-#         'HOST': os.environ["PGHOST"],
-#         'PORT': os.environ["PGPORT"],
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["PGDATABASE"],
+        'USER': os.environ["PGUSER"],
+        'PASSWORD': os.environ["PGPASSWORD"],
+        'HOST': os.environ["PGHOST"],
+        'PORT': os.environ["PGPORT"],
+    }
+}
 
-import dj_database_url
-
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'),conn_max_age=600)}
+# import dj_database_url
+#
+# DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'),conn_max_age=600)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
